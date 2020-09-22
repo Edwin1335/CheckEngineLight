0dd80189d38db67cd3f9541403aef5fb005a5a95
ravinamine <ravinamine@gmail.com>
2020-09-22 18:48:15 -0500

    22 September, 2020 Movement Scripting

---
+++ CheckEngine/Assets/motion_work/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<RigidBody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal"); //Built in Unity input field
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
