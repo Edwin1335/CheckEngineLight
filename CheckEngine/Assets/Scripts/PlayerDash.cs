22e13e5403a02145cee41498d0d81d85b66bb508
ravinamine <ravinamine@gmail.com>
2020-10-12 01:36:54 -0500

    Commented movement scripts

--- CheckEngine/Assets/Scripts/PlayerDash.cs
+++ CheckEngine/Assets/Scripts/PlayerDash.cs
@@ -14,24 +14,33 @@ public class PlayerDash : MonoBehaviour
 
     private void Awake(){
         _rigidBody = GetComponent<Rigidbody2D>();
+
+        //Player dashes for a certain time (_startDashTime), given in Unity inspector
         _dashTime = _startDashTime;
     }
 
     private void FixedUpdate(){
+
+        //When current dash time goes below zero, indicating that the timeer has ended.
+        //Resets player movement direction and velocity to zero and sets current dash time back to full dash time length.
         if (_dashTime <= 0){
             _direction = 0;
             _dashTime = _startDashTime;
             _rigidBody.velocity = Vector2.zero;
         }
         else{
+            //Performs dash when a direction is being pressed
             if (_direction != 0){
+                //Begins counting down until current dash time reaches zero
                 _dashTime -= Time.deltaTime;
+                //Applies high force to player to greatly increase their movement speed for dash duration
                 _rigidBody.AddForce(new Vector2(_direction * _dashSpeed, _rigidBody.velocity.y));
             }
         }
     }
 
     public void Dash(float _input){
+        //Passes directional input from PlayerController script
         _direction = _input;
     }
 }
--- CheckEngine/Assets/Scripts/PlayerMovement.cs
+++ CheckEngine/Assets/Scripts/PlayerMovement.cs
@@ -16,6 +16,8 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void Update(){
+        //Flips player direction depending on what arrow key is pressed.
+        //If right is pressed, then the player will face right, and vice versa.
         float _dirSign = transform.localScale.x;
         if ((_direction > 0 && _dirSign < 0) || (_direction < 0 && _dirSign > 0)){
             transform.localScale *= new Vector2(-1, 1);
@@ -23,11 +25,14 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void FixedUpdate(){
+        //Applies fixed velocity movement to player
+        //_moveSpeed can be defined in Unity Editor
         _rigidBody.velocity = new Vector2(_direction * _moveSpeed, _rigidBody.velocity.y);
 
     }
 
     public void Move(float _input){
+        //Passes directional input from other scripts to this script.
         _direction = _input;
     }
 }
