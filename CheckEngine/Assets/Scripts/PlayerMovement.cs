daaaa0565f23bea21e7d5b0d7a77f8ecde2dcf21
smaredia11 <55557452+smaredia11@users.noreply.github.com>
2020-10-13 14:29:38 -0500

    Revert "Revert "Merge branch 'master' of https://github.com/Edwin1335/CheckEngineLight""
    This reverts commit 29d0bd09cb006569ff328adc083bea18c9c95bb2.

--- CheckEngine/Assets/Scripts/PlayerMovement.cs
+++ CheckEngine/Assets/Scripts/PlayerMovement.cs
@@ -10,9 +10,11 @@ public class PlayerMovement : MonoBehaviour
     private bool _playerDirection;
 
     private Rigidbody2D _rigidBody;
+    Animator _animator;
 
     private void Awake(){
         _rigidBody = GetComponent<Rigidbody2D>();
+        _animator = gameObject.GetComponent<Animator>();
     }
 
     private void Update(){
