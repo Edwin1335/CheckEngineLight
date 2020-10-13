29d0bd09cb006569ff328adc083bea18c9c95bb2
smaredia11 <55557452+smaredia11@users.noreply.github.com>
2020-10-13 14:29:34 -0500

    Revert "Merge branch 'master' of https://github.com/Edwin1335/CheckEngineLight"
    This reverts commit 9480c7d42700450c93d578d8a602e6f62782491b, reversing
    changes made to 537b35a14905dd17777d51c0e7184d33ae83022b.

--- CheckEngine/Assets/Scripts/PlayerWallJump.cs
+++ CheckEngine/Assets/Scripts/PlayerWallJump.cs
@@ -17,22 +17,34 @@ public class PlayerWallJump : MonoBehaviour
 
     [SerializeField]
     private float _wallSlidingSpeed;
+    private float _wallInput;
     private float _dirInput;
-    private bool _jumpKey;
+    private bool _jumpInput;
 
     private Rigidbody2D _rigidBody;
-    //private Collider2D _collider;
+    private Collider2D _collider;
     private PlayerJump _jumping;
 
     private void Awake(){
         _rigidBody = GetComponent<Rigidbody2D>();
-        //_collider = GetComponent<Collider2D>();
+        _collider = GetComponent<Collider2D>();
         _jumping = GetComponent<PlayerJump>();
     }
 
     private void Update()
     {
-        //Throws hitbox detection in "front" of player instead of downward to allow for wall sliding.
+        /*
+        //Throws hitbox detection ray in "front" of player instead of downward to allow for wall sliding.
+        Vector2 _raycastDirection = new Vector2(_dirInput, 0);
+        RaycastHit2D _front = Physics2D.Raycast(transform.position, _raycastDirection, _raycastFront, _groundLayer);
+        if (_front.collider != null){
+            _touchingFront = true;
+        }
+        else{
+            _touchingFront = false;
+        }
+        */
+
         _touchingFront = Physics2D.OverlapCircle(_front.position, checkRadius, _groundLayer);
 
         //If player's "front" is against a wall, slow down fall velocity.
@@ -40,21 +52,16 @@ public class PlayerWallJump : MonoBehaviour
         if (_touchingFront == true && _dirInput != 0){
             _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, Mathf.Clamp(_rigidBody.velocity.y, -_wallSlidingSpeed, float.MaxValue));
             _isSliding = true;
-            
         }
         else{
             _isSliding = false;
-        }
-
-        if (_isSliding == true && _jumpKey == true){
-            _jumping.Jump(_jumpKey, _isSliding, _dirInput);
         }
     }
 
     public void SlideJump(bool _jumpKeyState, float _input){
         //Passes jump key state and direction from other scripts to this script.
         //Debug.Log("Slide Triggered");
-        _jumpKey = _jumpKeyState;
+        _jumpInput = _jumpKeyState;
         _dirInput = _input;
     }
 }
