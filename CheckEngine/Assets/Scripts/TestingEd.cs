29d29a784fb5c1de0d3d1575f6ef8b855413ead5
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2020-10-13 01:57:40 -0500

    Created Demos Scene for Duel

--- CheckEngine/Assets/Scripts/TestingEd.cs
+++ CheckEngine/Assets/Scripts/TestingEd.cs
@@ -1,27 +1,91 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class TestingEd : MonoBehaviour
 {
-    public float speed;
-    private float moveInput;
-    private bool facingRight;
-    private Rigidbody2D rb;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce = 0;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
 
+    private float moveInputHorizontal;
+    private bool facingRight;
+    private bool isGrounded;
+    private bool pressedJumpButton;
+    private bool playerIsFalling;
+
+    private Rigidbody2D rb;
     private Animator animator;
 
+    // Start is used to get components at the start of the scene.
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
 
+    //Update is used to check input
+    private void Update()
+    {
+        // Check if player presses the <- or -> buttons.
+        moveInputHorizontal = Input.GetAxisRaw("Horizontal");
+        // Check if player is on touching the ground.
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        // Check if player presses the UP button.
+        if (isGrounded && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            pressedJumpButton = true;
+        }
+    }
+
+    // Fixed updates is used to preform operations and animations on input.
     private void FixedUpdate()
     {
-        moveInput = Input.GetAxisRaw("Horizontal");
-        Debug.Log(moveInput);
-        if (moveInput != 0)
+        // Funtion to move the player left and right and animate him.
+        movePlayer(moveInputHorizontal);
+        
+        // Funtion to make the player Jump.
+        if (pressedJumpButton)
+        {
+            playerJump();
+        }
+
+        if(pressedJumpButton && isGrounded)
+        {
+            animator.SetBool("isJumping", false);
+            pressedJumpButton = false;
+        }
+
+        // If the player is falling, play animation.
+        if (!isGrounded && rb.velocity.y < -0.1)
+        {
+            animator.SetBool("isFalling", true);
+            playerIsFalling = true;
+        }
+        else
+        {
+            animator.SetBool("isFalling", false);
+
+        }
+
+        // If falling and landed, play landin animation
+        if(playerIsFalling && isGrounded)
+        {
+            playerIsFalling = false;
+            animator.SetTrigger("hasLanded");
+        }
+    }
+
+    private void movePlayer(float horizInput)
+    {
+        // Display horizontal input
+        Debug.Log("Horsizontal input " + moveInputHorizontal);
+        // Move the player 
+        rb.velocity = new Vector2(moveInputHorizontal * speed, rb.velocity.y);
+
+        // Perform animations if moveing
+        if ((moveInputHorizontal * speed) != 0)
         {
             animator.SetBool("isRunning", true);
         }
@@ -29,23 +93,31 @@ public class TestingEd : MonoBehaviour
         {
             animator.SetBool("isRunning", false);
         }
-        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
-        if (facingRight == false && moveInput < 0)
+        // Flip player left or right based on direction hes looking at.
+        if (facingRight == false && moveInputHorizontal < 0)
         {
             flipPLayer();
         }
-        else if (facingRight == true && moveInput > 0)
+        else if (facingRight == true && moveInputHorizontal > 0)
         {
             flipPLayer();
         }
     }
 
-    void flipPLayer()
+    private void flipPLayer()
     {
         facingRight = !facingRight;
         Vector3 Scaler = transform.localScale;
         Scaler.x *= -1;
         transform.localScale = Scaler;
     }
+
+    private void playerJump()
+    {
+        rb.velocity = Vector2.up * jumpForce;
+        animator.SetTrigger("takeOff");
+        animator.SetBool("isJumping", true);
+    }
+
 }
