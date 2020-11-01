c95c30694df9ad9cb804474d313e2f97588d54dd
smaredia11 <55557452+smaredia11@users.noreply.github.com>
2020-10-31 23:11:20 -0500

    fixing player issue

---
+++ CheckEngine/Assets/Scripts/Pikeem_AI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pikeem_AI : MonoBehaviour
+{
+    [SerializeField] float pikeemMovespeed = 1f;
+
+    Rigidbody2D pkrigbody;
+
+    void Start()
+    {
+        pkrigbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (IsFacingRight())
+        {
+            pkrigbody.velocity = new Vector2(pikeemMovespeed, 0f);
+        }
+        else
+        {
+            pkrigbody.velocity = new Vector2(-pikeemMovespeed, 0f);
+        }
+    }
+
+    private bool IsFacingRight()
+    {
+        return transform.localScale.x > Mathf.Epsilon;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(pkrigbody.velocity.x)), transform.localScale.y);
+    }
+}
