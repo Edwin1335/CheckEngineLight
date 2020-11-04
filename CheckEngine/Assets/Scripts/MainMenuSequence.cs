68faa8499d2f8b5348235dcad33e5fe44f7a0c87
Daniel Aguila <47072469+Daniel-Aguila@users.noreply.github.com>
2020-11-04 00:04:28 -0600

    Add files via upload

---
+++ CheckEngine/Assets/Scripts/MainMenuSequence.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuSequence : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadVideos());
+    }
+
+    // Update is called once per frame
+    IEnumerator LoadVideos()
+    {
+        Handheld.PlayFullScreenMovie("MainMenu_background_video", Color.black, FullScreenMovieControlMode.CancelOnInput);
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
+
+        //second vid
+        Handheld.PlayFullScreenMovie("Intro", Color.black, FullScreenMovieControlMode.CancelOnInput);
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
+    }
+}
---
+++ CheckEngine/Assets/Scripts/QuitMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void quit()
+    {
+        Application.Quit();
+        Debug.Log("Game Over");
+    }
+}
