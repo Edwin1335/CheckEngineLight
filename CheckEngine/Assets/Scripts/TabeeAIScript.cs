8c44055ef7faeef1cd549e05ef73f1272b851d02
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2020-11-09 04:47:16 -0600

    Tabee AI - Returns to previous position

--- CheckEngine/Assets/Scripts/TabeeAIScript.cs
+++ CheckEngine/Assets/Scripts/TabeeAIScript.cs
@@ -8,10 +8,13 @@ public class TabeeAIScript : MonoBehaviour
     public float lineOfSight;
     public float attackRange;
     private bool playerFound = false;
+    private bool flyTowardsPlayer = false;
 
     // Get Components
     private Animator animator;
     private Transform player;
+    private Vector2 originalPosition;
+
     void Start()
     {
         // Check if player exists in the scene.
@@ -20,6 +23,7 @@ public class TabeeAIScript : MonoBehaviour
             player = GameObject.FindGameObjectWithTag("Player").transform;
             playerFound = true;
             animator = GetComponent<Animator>();
+            originalPosition = new Vector2(this.transform.position.x, this.transform.position.y);
         }
     }
 
@@ -32,8 +36,15 @@ public class TabeeAIScript : MonoBehaviour
 
             if (distanceFromPlayer < lineOfSight && distanceFromPlayer > attackRange)
             {
+                flyTowardsPlayer = true;
                 animator.SetBool("isAttacking", false);
                 transform.position = Vector2.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
+            }
+            else if (distanceFromPlayer > lineOfSight && distanceFromPlayer > attackRange)
+            {
+                // Move tabbee back to where he was.
+                flyTowardsPlayer = false;
+                transform.position = Vector2.MoveTowards(this.transform.position, originalPosition, (speed / 2) * Time.deltaTime);
             }
             else if (distanceFromPlayer <= attackRange)
             {
@@ -43,14 +54,29 @@ public class TabeeAIScript : MonoBehaviour
                 //attack player once every x amount of secs
             }
 
-            // Flip tabbe to face player.
-            if (this.transform.position.x < player.position.x)
+            // Flip Tabee to face player.
+            if (flyTowardsPlayer)
             {
-                this.transform.localScale = new Vector2(1, 1);
+                if (this.transform.position.x < player.position.x)
+                {
+                    this.transform.localScale = new Vector2(1, 1);
+                }
+                else if (this.transform.position.x > player.position.x)
+                {
+                    this.transform.localScale = new Vector2(-1, 1);
+                }
             }
-            else if (this.transform.position.x > player.position.x)
+            else
             {
-                this.transform.localScale = new Vector2(-1, 1);
+                if (this.transform.position.x < originalPosition.x)
+                {
+                    this.transform.localScale = new Vector2(1, 1);
+                }
+                else if (this.transform.position.x > originalPosition.x)
+                {
+                    this.transform.localScale = new Vector2(-1, 1);
+                }
+
             }
         }
 
