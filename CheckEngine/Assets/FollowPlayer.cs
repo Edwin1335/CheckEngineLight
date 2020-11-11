24e290e7f7ef859e3088d078f84998bbbd8c0a76
smaredia11 <55557452+smaredia11@users.noreply.github.com>
2020-11-10 20:21:28 -0600

    minor changes gloomy light object health fire prefab

---
+++ CheckEngine/Assets/FollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(player.position.x, player.position.y, -1.25f), 1000);
+    }
+}
