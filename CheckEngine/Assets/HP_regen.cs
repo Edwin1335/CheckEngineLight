efbeb8c57535ff1820c0e3fa1d1f690ef3abe4cc
smaredia11 <55557452+smaredia11@users.noreply.github.com>
2020-11-12 16:01:31 -0600

    health

---
+++ CheckEngine/Assets/HP_regen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP_regen : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        GameObject thePlayer = GameObject.Find("Gloomy");
+        HealthSystem SN = thePlayer.GetComponent<HealthSystem>();
+
+        if (col.CompareTag("Player") && SN.health != 5)
+        {
+            SN.health = 5;
+        }
+    }
+}
