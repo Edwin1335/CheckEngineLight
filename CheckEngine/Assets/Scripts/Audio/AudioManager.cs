1383f4b191ce6fc76614a8ad0463ff604bb16bd7
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2020-12-08 05:35:13 -0600

    Audio manager created

---
+++ CheckEngine/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    private void Start() 
+    {
+        
+    }
+
+    public void Play(String name)
+    {
+        Sound s = Array.Find(sounds, sounds => sounds.name == name);
+        if (s == null)
+            return;
+        s.source.Play();
+    }
+
+}
