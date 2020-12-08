63b740961d1871fc40e69e9eff511c0c13d8352c
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2020-12-07 22:12:14 -0600

    Attack trigger script created

---
+++ CheckEngine/Assets/Scripts/KitCat Test/AttackTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackTrigger : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        List<Collider2D> recentDamageList = new List<Collider2D>();
+        recentDamageList.Clear();
+
+        if (col.tag == "Enemy")
+        {
+            if (!recentDamageList.Contains(col))
+            {
+                recentDamageList.Add(col);
+                col.SendMessageUpwards("EnemyDamage", 1);
+                Debug.Log("Attacked" + col.name);
+            }
+        }
+    }
+}
