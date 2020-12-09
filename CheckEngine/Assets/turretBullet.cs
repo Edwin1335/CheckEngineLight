6c2b42c01649102d8a486cec4212718b56ffa3f8
smaredia11 <55557452+smaredia11@users.noreply.github.com>
2020-12-09 14:47:29 -0600

    level 90% done

---
+++ CheckEngine/Assets/turretBullet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turretBullet : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    public GameObject impactEffect;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+
+        GameObject thePlayer = GameObject.FindGameObjectWithTag("Player");
+        HealthSystem hs = thePlayer.GetComponent<HealthSystem>();
+
+        if (coll.CompareTag("Player"))
+        {
+            DestroyProj();
+            hs.health -= 1;
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    void DestroyProj()
+    {
+        Destroy(gameObject);
+        Instantiate(impactEffect, transform.position, transform.rotation);
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
