30f8c8cc0f42bebd3153140427082cba05bb5e8e
smaredia11 <55557452+smaredia11@users.noreply.github.com>
2020-12-10 15:38:25 -0600

    adding lever

---
+++ CheckEngine/Assets/LeverScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverScript : MonoBehaviour
+{
+    public GameObject platform;
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown("e"))
+            {
+                
+            }
+        }
+    }
+}
---
+++ CheckEngine/Assets/lever_platform.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lever_platform : MonoBehaviour
+{
+    public Transform pos1, pos2;
+    public float speed;
+    public Transform startPos;
+    public bool on = false;
+    Vector3 nextPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPos = startPos.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (on == true)
+        {
+            if (transform.position == pos1.position)
+            {
+                nextPos = pos2.position;
+            }
+            if (transform.position == pos2.position)
+            {
+                nextPos = pos1.position;
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.transform.tag == "Player")
+        {
+            other.collider.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.collider.transform.tag == "Player")
+        {
+            other.collider.transform.SetParent(null);
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(pos1.position, pos2.position);
+    }
+}
+
