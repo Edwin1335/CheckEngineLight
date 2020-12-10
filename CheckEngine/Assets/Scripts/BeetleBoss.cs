4c4b87cb6575c4edb5c11b975f1890f66d913c74
Ravi Namuduri <22359412+ravinamine@users.noreply.github.com>
2020-12-10 11:56:17 -0600

    Boss Testing

--- CheckEngine/Assets/Scripts/BeetleBoss.cs
+++ CheckEngine/Assets/Scripts/BeetleBoss.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class BeetleBoss : MonoBehaviour
 {
     //Modifiable Variables
-    [SerializeField] private float _speed;
-    [SerializeField] private float _lineOfSight;
-    [SerializeField] private float _attackRange;
+    [SerializeField] private float _speed = 0.5f;
+    [SerializeField] private float _lineOfSight = 10;
+    [SerializeField] private float _attackRange = 5;
 
     //Hidden Variables
     private bool _playerFound = false;
@@ -16,80 +16,90 @@ public class BeetleBoss : MonoBehaviour
 
     //Get Components
     private Animator animator;
-    private Transform _player;
+    // private Transform _player;
     private Vector2 _originalPosition;
     private TakeDamage _damage;
+
+    private GameObject _player;
 
     private Rigidbody2D _rigidBody;
     private Collider2D _collider;
 
     private void Awake(){
-        if (GameObject.FindGameObjectWithTag("Player")){
-            // _damage = GetComponent<TakeDamage>();
-            // _damage.enemyName = "Tabee";
-            _player = GameObject.FindGameObjectWithTag("Player").transform;
-            _playerFound = true;
-            animator = GetComponent<Animator>();
-            _originalPosition = new Vector2(this.transform.position.x, this.transform.position.y);
-        }
+        // if (GameObject.FindGameObjectWithTag("Player")){
+        //     _damage = GetComponent<TakeDamage>();
+        //     _damage.enemyName = "Tabee";
+        //     _player = GameObject.FindGameObjectWithTag("Player").transform;
+        //     _playerFound = true;
+        //     animator = GetComponent<Animator>();
+        //     _originalPosition = new Vector2(this.transform.position.x, this.transform.position.y);
+        // }
+        _player = GameObject.Find("Gloomy");
 
-        _rigidBody.GetComponent<Rigidbody2D>();
-        _collider.GetComponent<Collider2D>();
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<Collider2D>();
     }
 
-    private void Update()
+    private void  FixedUpdate()
     {
-        if (_playerFound && !_attacked)
-        {
-            float _distanceFromPlayer = Vector2.Distance(_player.position, transform.position);
-
-            if (_distanceFromPlayer < _lineOfSight && _distanceFromPlayer > _attackRange)
-            {
-                _flyTowardsPlayer = true;
-                animator.SetBool("isAttacking", false);
-                transform.position = Vector2.MoveTowards(new Vector2(this.transform.position.x, 0), _player.position, _speed * Time.deltaTime);
-            }
-            else if (_distanceFromPlayer > _lineOfSight && _distanceFromPlayer > _attackRange)
-            {
-                // Move Boss back to where he was.
-                _flyTowardsPlayer = false;
-                transform.position = Vector2.MoveTowards(this.transform.position, _originalPosition, (_speed / 2) * Time.deltaTime);
-            }
-            else if (_distanceFromPlayer <= _attackRange)
-            {
-                // Attack Animation.
-                animator.SetBool("isAttacking", true);
-
-                // Must still move towards the player so that it could collide and the player can take damage on collision.
-                transform.position = Vector2.MoveTowards(this.transform.position, _player.position, _speed * Time.deltaTime);
-                //reduce player health
-                //attack player once every x amount of secs
-            }
-
-            // Flip Boss to face player or previous position.
-            if (_flyTowardsPlayer)
-            {
-                if (this.transform.position.x < _player.position.x)
-                {
-                    this.transform.localScale = new Vector2(1, 1);
-                }
-                else if (this.transform.position.x > _player.position.x)
-                {
-                    this.transform.localScale = new Vector2(-1, 1);
-                }
-            }
-            else
-            {
-                if (this.transform.position.x < _originalPosition.x)
-                {
-                    this.transform.localScale = new Vector2(1, 1);
-                }
-                else if (this.transform.position.x > _originalPosition.x)
-                {
-                    this.transform.localScale = new Vector2(-1, 1);
-                }
-
-            }
+        transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, _speed * Time.deltaTime);
+        if (this.transform.position.x < _player.transform.position.x){
+            this.transform.localScale = new Vector2(1, 1);
         }
+        else if (this.transform.position.x > _player.transform.position.x){
+            this.transform.localScale = new Vector2(-1, 1);
+        }
+        // if (_playerFound && !_attacked)
+        // {
+        //     float _distanceFromPlayer = Vector2.Distance(_player.position, transform.position);
+
+        //     if (_distanceFromPlayer < _lineOfSight && _distanceFromPlayer > _attackRange)
+        //     {
+        //         _flyTowardsPlayer = true;
+        //         animator.SetBool("isAttacking", false);
+        //         transform.position = Vector2.MoveTowards(new Vector2(this.transform.position.x, 0), _player.position, _speed * Time.deltaTime);
+        //     }
+        //     else if (_distanceFromPlayer > _lineOfSight && _distanceFromPlayer > _attackRange)
+        //     {
+        //         // Move Boss back to where he was.
+        //         _flyTowardsPlayer = false;
+        //         transform.position = Vector2.MoveTowards(this.transform.position, _originalPosition, (_speed / 2) * Time.deltaTime);
+        //     }
+        //     else if (_distanceFromPlayer <= _attackRange)
+        //     {
+        //         // Attack Animation.
+        //         animator.SetBool("isAttacking", true);
+
+        //         // Must still move towards the player so that it could collide and the player can take damage on collision.
+        //         transform.position = Vector2.MoveTowards(this.transform.position, _player.position, _speed * Time.deltaTime);
+        //         //reduce player health
+        //         //attack player once every x amount of secs
+        //     }
+
+        //     // Flip Boss to face player or previous position.
+        //     if (_flyTowardsPlayer)
+        //     {
+        //         if (this.transform.position.x < _player.position.x)
+        //         {
+        //             this.transform.localScale = new Vector2(1, 1);
+        //         }
+        //         else if (this.transform.position.x > _player.position.x)
+        //         {
+        //             this.transform.localScale = new Vector2(-1, 1);
+        //         }
+        //     }
+        //     else
+        //     {
+        //         if (this.transform.position.x < _originalPosition.x)
+        //         {
+        //             this.transform.localScale = new Vector2(1, 1);
+        //         }
+        //         else if (this.transform.position.x > _originalPosition.x)
+        //         {
+        //             this.transform.localScale = new Vector2(-1, 1);
+        //         }
+
+        //     }
+        // }
     }
 }
