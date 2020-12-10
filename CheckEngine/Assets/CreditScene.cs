7fec96f82b35c7f13c32ce631b6d19d6a2f1924e
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2020-12-10 17:11:59 -0600

    Pushing scene changes

---
+++ CheckEngine/Assets/CreditScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CreditScene : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
--- CheckEngine/Assets/biggemscript.cs
+++ CheckEngine/Assets/biggemscript.cs
@@ -1,10 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class biggemscript : MonoBehaviour
 {
     public GameObject gameover;
+    private bool loadNew = false;
+    private float time;
+
+    private void Update() 
+    {
+        if(loadNew)
+        {
+            if(Time.time >= time + 20.0f)
+            {
+                SceneManager.LoadScene(2);
+            }
+        }
+    }
+    
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.CompareTag("Player"))
@@ -13,8 +28,9 @@ public class biggemscript : MonoBehaviour
             FindObjectOfType<AudioManager>().Play("Collect");
             Destroy(gameObject);
             gameover.SetActive(true);
+            loadNew = true;
+            time = Time.time;
+            SceneManager.LoadScene(2);
         }
-
-
     }
 }
